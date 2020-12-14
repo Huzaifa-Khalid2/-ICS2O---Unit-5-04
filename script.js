8ef5299eda3ddbcfa599f81b27a0ce57bd6474a6
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-14 16:05:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let side1 = 'five centimeters'
+ side1 = 'side a = 5cm';
+
+alert(side1); // shows the variable content
+ 
+ var side2 = 'five centimeters'
+ side2 = 'side b = 5cm';
+
+alert(side2); // shows the variable content
+
+var height = 'five centimeters'
+height = 'h = 5cm';
+
+alert(height); // shows the variable content
+
+let a = 'area of trapezoid' 
+a = 'a = area of trapezoid';
+
+alert(a); // shows the variable content
+
+ var formula = 'a = (a + b)/2 x h'
+ formula = 'a = (side a + side b)/2 x h'
+
+alert(formula); // shows the variable content
+
+let equation = 'a = (a + b)/2 x h'
+equation = 'a = (5+5)/2 x 5'
+
+alert(equation); // shows the variable content
+
+var area = 'a = (5+5)/2 x 5'
+area = 'a = 25cm2'
+
+alert(area); // shows the variable content
+
+let statement = 'Therefore the area of this trapezoid is 25cm'
+statement = 'Therefore the area of this trapezoid is 25cm2'
+
+alert(statement); // shows the variable content
