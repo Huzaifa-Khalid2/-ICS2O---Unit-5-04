bc4c19a2e5f4d4a2857b2ef335d0fc787b041ae8
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-20 21:23:55 +0000

    fixed javascript format

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 let side1 = 'five centimeters'
- side1 = 'side a = 5cm';
-
-alert(side1); // shows the variable content
- 
- var side2 = 'five centimeters'
- side2 = 'side b = 5cm';
-
-alert(side2); // shows the variable content
-
-var height = 'five centimeters'
-height = 'h = 5cm';
-
-alert(height); // shows the variable content
-
+side1 = 'side a = 5cm'
+alert(side1) 
+let side2 = 'five centimeters'
+side2 = 'side b = 5cm'
+alert(side2)
+let height = 'five centimeters'
+height = 'h = 5cm'
+alert(height) 
 let a = 'area of trapezoid' 
-a = 'a = area of trapezoid';
-
-alert(a); // shows the variable content
-
- var formula = 'a = (a + b)/2 x h'
- formula = 'a = (side a + side b)/2 x h'
-
-alert(formula); // shows the variable content
-
+a = 'a = area of trapezoid'
+alert(a)
+let formula = 'a = (a + b)/2 x h'
+formula = 'a = (side a + side b)/2 x h'
+alert(formula) 
 let equation = 'a = (a + b)/2 x h'
-equation = 'a = (5+5)/2 x 5'
-
-alert(equation); // shows the variable content
-
-var area = 'a = (5+5)/2 x 5'
-area = 'a = 25cm2'
-
-alert(area); // shows the variable content
-
+equation = ' = (5+5)/2 x 5'
+alert(equation) 
+let area = 'a = (5+5)/2 x 5'
+area = ' = 25cm2'
+alert(area) 
 let statement = 'Therefore the area of this trapezoid is 25cm'
 statement = 'Therefore the area of this trapezoid is 25cm2'
-
-alert(statement); // shows the variable content
+alert(statement)
