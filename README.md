4bd29af262c389543fdc846a18c8e255a89e6e40
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-30 19:07:52 +0000

    fixed trailing spaces and added css content

--- README.md
+++
@@ -1,2 +0,0 @@
-# -ICS2O---Unit-5-04
- [![linter](https://github.com/<OWNER>/<REPOSITORY>/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter)   
--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let side1 = 'five centimeters'
 side1 = 'side a = 5cm'
-alert(side1) 
+alert(side1)
 let side2 = 'five centimeters'
 side2 = 'side b = 5cm'
 alert(side2)
 let height = 'five centimeters'
 height = 'h = 5cm'
-alert(height) 
-let a = 'area of trapezoid' 
+alert(height)
+let a = 'area of trapezoid'
 a = 'a = area of trapezoid'
 alert(a)
 let formula = 'a = (a + b)/2 x h'
 formula = 'a = (side a + side b)/2 x h'
-alert(formula) 
+alert(formula)
 let equation = 'a = (a + b)/2 x h'
 equation = ' = (5+5)/2 x 5'
-alert(equation) 
+alert(equation)
 let area = 'a = (5+5)/2 x 5'
 area = ' = 25cm2'
-alert(area) 
+alert(area)
 let statement = 'Therefore the area of this trapezoid is 25cm'
 statement = 'Therefore the area of this trapezoid is 25cm2'
 alert(statement)
